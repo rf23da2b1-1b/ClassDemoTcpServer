b1e2efce0e3eb6cf53a027d90544e173d49def83
Peter Levinsky <pele@zealand.dk>
2024-08-29 13:43:25 +0200

    Add project files.

---
+++ ClassDemoTcpServer/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassDemoTcpServer;
+
+Console.WriteLine("Hello, World!");
+
+
+Server server = new Server();
+server.Start();
+
+Console.ReadKey();
---
+++ ClassDemoTcpServer/Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoTcpServer
+{
+    internal class Server
+    {
+        private const int PORT = 7;
+
+        public void Start()
+        {
+            // definerer server med port nummer
+            TcpListener server = new TcpListener(PORT);
+            server.Start();
+            Console.WriteLine("Server startet på port " + PORT);
+
+            // venter på en klient 
+            TcpClient socket = server.AcceptTcpClient();
+
+
+            // åbner for tekst strenge
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(socket.GetStream());
+
+            // læser linje fra nettet
+            string l = sr.ReadLine();
+
+            Console.WriteLine("Modtaget: " + l);
+            // skriver linje tilbage
+            sw.WriteLine(l);
+            sw.Flush();
+
+        }
+    }
+}
